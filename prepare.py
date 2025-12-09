de1e615664e974fab4161e102806325bef509d91
Cristian Le <git@lecris.dev>
2025-12-09 10:14:52 +0700

    Improve testing-farm error (#4)

--- prepare.py
+++ prepare.py
@@ -94,10 +94,17 @@ if __name__ == "__main__":
     repo_path: Path = args.workdir / "repo"
     repo_path.mkdir(exist_ok=True)
 
-    match args.action:
-        case "koji-task":
-            koji_task(args, repo_path)
-        case "bodhi-update":
-            bodhi_update(args, repo_path)
-        case _:
-            raise NotImplementedError
+    try:
+        match args.action:
+            case "koji-task":
+                koji_task(args, repo_path)
+            case "bodhi-update":
+                bodhi_update(args, repo_path)
+            case _:
+                raise NotImplementedError
+    except subprocess.CalledProcessError:
+        logger.error("Prepare failed")
+        raise SystemExit(1)
+    except Exception as exc:
+        logger.error("Unexpected prepare failure", exc_info=exc)
+        raise SystemExit(2)
